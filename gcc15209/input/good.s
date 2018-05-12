3516c0780bce44adc05d4075b2f825bdb73e7d4a
linhai.song <songlinhai0543@gmail.com>
2018-05-12 11:45:32 -0700

    remove gcc bugs

--- gcc15209/input/good.s
+++
@@ -1,3 +0,0 @@
-	.file	"good.ii"
-	.section	.note.GNU-stack,"",@progbits
-	.ident	"GCC: (GNU) 3.4.0"
--- gcc27733/inputs/2.c
+++
@@ -1,4 +0,0 @@
-unsigned long long foo( unsigned long long zz)  
-{
- return zz * 15238614669586151335ULL;
-}
